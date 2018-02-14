2b90ae512c18582e594b1410674d52b10bb6f11a
DenisGubeev <35633718+DenisGubeev@users.noreply.github.com>
2018-02-14 19:18:20 +0300

    Update list.php

--- list.php
+++ list.php
@@ -1,1 +1,24 @@
-
+<?php
+    $allFiles = glob('tests/*.json');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>List</title>
+</head>
+<body>
+    <a href="admin.php" class="back"><div>Назад</div></a>
+    <hr>
+    <?php if (!empty($allFiles)): ?>
+        <?php foreach ($allFiles as $file): ?>
+            <div>
+                <h1><?php echo str_replace('tests/', '', $file); ?></h1><br>
+                <em>Загружен: <?php echo date("d-m-Y", filemtime($file)) ?></em><br>
+                <a href="test.php?number=<?php echo array_search($file, $allFiles); ?>">Перейти на страницу с тестом ></a>
+            </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
+    <?php if (empty($allFiles)) echo 'Пока не загружено ни одного теста';?>
+</body>
+</html>
